1ff024b9ee039895b367f86dd9b28c7c5677b3b4
milcab <milcarodriguez23@gmail.com>
2021-08-23 15:22:15 -0400

    roll dice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,71 @@
 // TODO: Declare any global variables we need
 document.addEventListener('DOMContentLoaded', function () {
-  // This is just a sanity check to make sure your JavaScript script is getting loaded
-  // You can remove it once you see it in your browser console in the developer tools
-  console.log('Hi')
+    // This is just a sanity check to make sure your JavaScript script is getting loaded
+    // You can remove it once you see it in your browser console in the developer tools
+    console.log('Hi')
 
-  // TODO: Add event listener and handler for flip and clear buttons
-  const flipButton = document.querySelector('#flip');
-  const clearButton = document.querySelector('#clear');
+    // TODO: Add event listener and handler for flip and clear buttons
+    const flipButton = document.querySelector('#flip');
+    const clearButton = document.querySelector('#clear');
 
-  let tailsCount = 0;
-  let headsCount = 0;
-  let total = 0;
-  let headsPercentage = 0;
-  let tailsPercentage = 0;
+    // let tailsCount = 0;
+    //let headsCount = 0;
+    //let total = 0;
+    let diceNumbers = [
+        0, // 1
+        0, // 2
+        0, // 3
+        0, // 4
+        0, // 5
+        0 // 6
+    ];
 
-  flipButton.addEventListener('click', function () {
-    // Flip Button Click Handler
-    // TODO: Determine flip outcome
-    // TODO: Update image and status message in the DOM
-    if (Math.random() >= 0.5) {
-      const image = document.querySelector('#penny-image');
-      image.src = '/assets/images/penny-heads.jpg';
+    let total = 0;
 
-      const message = document.querySelector('#message');
-      message.innerHTML = 'Heads';
-
-      headsCount = headsCount + 1;
-    } else {
-      const image = document.querySelector('#penny-image');
-      image.src = '/assets/images/penny-tails.jpg'
-
-      const message = document.querySelector('#message');
-      message.innerHTML = 'Tails';
-      tailsCount = tailsCount + 1;
+    const getImageURL = function (index) {
+        return `/assets/images/dice/dice${index + 1}.png`
     }
 
-    // Update the scorboard
-    // TODO: Calculate the total number of rolls/flips
-    // Make variables to track the percentages of heads and tails
-    // TODO: Use the calculated total to calculate the percentages
-    // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-    // TODO: Update the display of each table cell
+    const getRandomImage = function () {
+        const randomIndex = Math.round(Math.random() * 5);
+        diceNumbers[randomIndex]++;
+        total++;
+        return getImageURL(randomIndex);
+    }
 
-    total = tailsCount + headsCount;
-    headsPercentage = Math.round((headsCount / total) * 100);
-    tailsPercentage = Math.round((tailsCount / total) * 100);
+    flipButton.addEventListener('click', function () {
+        const image = document.querySelector('#penny-image');
+        image.src = getRandomImage();
 
-    const headsElement = document.querySelector("#heads")
-    headsElement.innerHTML = headsCount;
+        for (let i = 0; i < diceNumbers.length; i++) {
+            const diceNumberCountElement = document.querySelector('#dice-number-count-' + i);
+            const diceNumberPercentageElement = document.querySelector('#dice-number-percentage-' + i);
 
-    const headsPercentageElement = document.querySelector("#heads-percent")
-    headsPercentageElement.innerHTML = `${headsPercentage}%`;
+            const count = diceNumbers[i];
+            const percentage = Math.round((count / total) * 100);
+            diceNumberCountElement.innerHTML = count;
+            diceNumberPercentageElement.innerHTML = `${percentage}%`;
+        }
+    });
 
-    const tailsElement = document.querySelector("#tails")
-    tailsElement.innerHTML = tailsCount;
+    clearButton.addEventListener('click', function () {
+        // Clear Button Click Handler
+        diceNumbers = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ];
 
-    const tailsPercentageElement = document.querySelector("#tails-percent")
-    tailsPercentageElement.innerHTML = `${tailsPercentage}%`;
-  });
+        total = 0;
 
-  clearButton.addEventListener('click', function () {
-    // Clear Button Click Handler
-    tailsCount = 0;
-    headsCount = 0;
-    total = 0;
-    headsPercentage = 0;
-    tailsPercentage = 0;
-    // TODO: Reset global variables to 0
-
-    // TODO: Update the scoreboard (same logic as in flip button click 
-    const headsElement = document.querySelector("#heads")
-    headsElement.innerHTML = 0;
-
-    const headsPercentageElement = document.querySelector("#heads-percent")
-    headsPercentageElement.innerHTML = '0%'
-
-    const tailsElement = document.querySelector("#tails")
-    tailsElement.innerHTML = 0;
-
-    const tailsPercentageElement = document.querySelector("#tails-percent")
-    tailsPercentageElement.innerHTML = '0%'
-
-    const image = document.querySelector('#penny-image');
-    image.src = '/assets/images/penny-heads.jpg';
-
-    const message = document.querySelector('#message');
-    message.innerHTML = 'Heads';
-  });
+        for (let i = 0; i < diceNumbers.length; i++) {
+            const diceNumberCountElement = document.querySelector('#dice-number-count-' + i);
+            const diceNumberPercentageElement = document.querySelector('#dice-number-percentage-' + i);
+            diceNumberCountElement.innerHTML = 0;
+            diceNumberPercentageElement.innerHTML = `0%`;
+        }
+    });
 })
